f094544fe5ab2313d503f1eb50da5ea28c6e98ea
TimoEvt <t.evteev@school1387.ru>
2025-11-10 18:52:41 +0400

    Refactor CSS alias in vitest configuration
    Updated CSS alias path for testing environment.

--- vitest.config.js
+++ vitest.config.js
@@ -1,17 +1,16 @@
+// vitest.config.js
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@hexlet/chatbot-v2/dist/init.css': path.resolve(__dirname, 'src/__mocks__/styleMock.js'),
-    },
-  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.js',
+    // Мокаем CSS из node_modules
+    alias: {
+      '@hexlet/chatbot-v2/dist/init.css': '/__mocks__/init.css', 
+    },
   },
 })
