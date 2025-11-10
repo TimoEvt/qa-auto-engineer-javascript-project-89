373b39dcc6ff97e8752ffd9124519ffde0663f83
Tim Evteev <timofey@Tims-MacBook-Air.local>
2025-11-10 19:28:42 +0400

    Update configs and mocks for tests

--- vitest.config.js
+++ vitest.config.js
@@ -1,3 +1,4 @@
+/// vitest.config.js
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
@@ -13,5 +14,18 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.js',
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    // Игнорируем CSS при импорте из node_modules
+    deps: {
+      inline: ['@hexlet/chatbot-v2']
+    },
+  },
+  css: {
+    modules: false, // чтобы Vitest не пытался парсить CSS
   },
 })
+
--- vitest.setup.js
+++ vitest.setup.js
@@ -2,3 +2,4 @@ import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 
 global.userEvent = userEvent
+import { vi } from 'vitest'; vi.mock('@hexlet/chatbot-v2/dist/init.css', () => ({}))
