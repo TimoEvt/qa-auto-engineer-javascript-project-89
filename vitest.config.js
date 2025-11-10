98840e55aa4b1cfb8b59d263c5e677131a9422be
TimoEvt <t.evteev@school1387.ru>
2025-11-10 18:57:55 +0400

    Add alias resolution for CSS imports

--- vitest.config.js
+++ vitest.config.js
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Мокаем CSS импорт из @hexlet/chatbot-v2
+      '@hexlet/chatbot-v2/dist/init.css': '/src/__mocks__/init.css',
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.js',
-    alias: {
-      '@hexlet/chatbot-v2/dist/init.css': '/__mocks__/init.css', 
-    },
   },
 })
