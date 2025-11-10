2bc3fb311271bad5524e1efe9018e019ab7f6716
TimoEvt <t.evteev@school1387.ru>
2025-11-10 18:40:55 +0400

    Import userEvent and add to global scope
    Added userEvent from testing-library to global scope.

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,6 +1,7 @@
 // vitest.setup.js
-import '@testing-library/jest-dom'
-import { vi } from 'vitest'
 
-// Мокаем CSS, чтобы Vitest не ругался на импорты стилей
-vi.mock('@hexlet/chatbot-v2/dist/init.css', () => ({}))
+import '@testing-library/jest-dom'
+
+import userEvent from '@testing-library/user-event'
+
+global.userEvent = userEvent
