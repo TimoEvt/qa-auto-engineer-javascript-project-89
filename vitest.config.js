281948a7be2149716796039d53cbd11f5215bcd9
TimoEvt <t.evteev@school1387.ru>
2025-11-10 18:38:57 +0400

    Add path alias for styleMock in Vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@hexlet/chatbot-v2/dist/init.css': path.resolve(__dirname, 'src/__mocks__/styleMock.js'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.js', // <- путь к твоему setup-файлу
+    setupFiles: './vitest.setup.js', // твой setup-файл
   },
 })
