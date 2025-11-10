58c88d14792c0218f64d1bc560695e2a7098f6b7
TimoEvt <t.evteev@school1387.ru>
2025-11-10 18:28:27 +0400

    Clean up comments in vitest.setup.js
    Removed comments related to jest-dom and CSS mocking.

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,7 +1,5 @@
-// Подключаем jest-dom для расширенных матчеров типа toBeInTheDocument
 import '@testing-library/jest-dom'
 
-// Мокаем CSS, чтобы Vitest не ругался на импорты стилей
 import { vi } from 'vitest'
 
 vi.mock('@hexlet/chatbot-v2/dist/init.css', () => ({}))
