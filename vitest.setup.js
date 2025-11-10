c8d657a5d142e863337e32997a6b6bf74f17f717
TimoEvt <t.evteev@school1387.ru>
2025-11-10 18:32:16 +0400

    Mock CSS import in vitest.setup.js
    Add mock for CSS import to prevent Vitest errors.

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,5 +1,6 @@
+// vitest.setup.js
 import '@testing-library/jest-dom'
-
 import { vi } from 'vitest'
 
+// Мокаем CSS, чтобы Vitest не ругался на импорты стилей
 vi.mock('@hexlet/chatbot-v2/dist/init.css', () => ({}))
