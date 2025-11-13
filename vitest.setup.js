0af09282925789358a6c31144c48d525773366a7
TimoEvt <t.evteev@school1387.ru>
2025-11-13 19:54:03 +0400

    Update vitest.setup.js

--- vitest.setup.js
+++ vitest.setup.js
@@ -2,20 +2,23 @@ import * as matchers from '@testing-library/jest-dom/matchers'
 import { expect, vi } from 'vitest'
 import React from 'react'
 
-// Расширяем глобальный expect
+// Расширяем глобальный expect для jest-dom matchers
 Object.assign(expect, matchers)
 
-// Мокаем пакет @hexlet/chatbot-v2
+// Мокаем пакет @hexlet/chatbot-v2 полностью, чтобы CSS не ломал тесты
 vi.mock('@hexlet/chatbot-v2', () => ({
   default: ({ steps }) =>
     React.createElement(
       'div',
       null,
-      steps.map(step => React.createElement('div', { key: step.id }, step.message)),
+      steps.map(step =>
+        React.createElement('div', { key: step.id }, step.message)
+      ),
       React.createElement(
         'button',
         { type: 'button', 'aria-label': 'Зарегистрироваться' },
         'Зарегистрироваться'
       )
     ),
+  init: () => {}, // если пакет экспортирует named function
 }))
