4cd086ce8cce11c91d6ccaa8838a8dd5166191cf
TimoEvt <t.evteev@school1387.ru>
2025-11-13 20:10:15 +0400

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,23 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // мок для всех CSS-файлов (в том числе из node_modules)
-      '\\.css$': path.resolve(__dirname, 'src/__mocks__/styleMock.js'),
-    },
-  },
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './vitest.setup.js',
     deps: {
       inline: ['@hexlet/chatbot-v2'],
-    },
-    transformMode: {
-      web: [/\.[jt]sx?$/],
     },
   },
 })
