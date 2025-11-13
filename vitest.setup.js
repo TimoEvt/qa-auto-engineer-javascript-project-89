d1783ba5e02f50ef69bbdb9f989c3734be79cdb6
TimoEvt <t.evteev@school1387.ru>
2025-11-13 18:31:12 +0400

    Update vitest.setup.js

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,17 +1,18 @@
-// src/vitest.setup.js
-import '@testing-library/jest-dom' // подключаем матчеры jest-dom
+import { expect } from 'vitest'
+import matchers from '@testing-library/jest-dom/matchers'
 import { vi } from 'vitest'
 import React from 'react'
 
-// Мокаем пакет @hexlet/chatbot-v2
+// подключаем jest-dom матчеры
+expect.extend(matchers)
+
+// мокаем пакет @hexlet/chatbot-v2
 vi.mock('@hexlet/chatbot-v2', () => ({
   default: ({ steps }) =>
     React.createElement(
       'div',
       null,
-      // рендерим все шаги
       steps.map(step => React.createElement('div', { key: step.id }, step.message)),
-      // добавляем кнопку "Зарегистрироваться"
-      React.createElement('button', { type: 'button' }, 'Зарегистрироваться')
+      React.createElement('button', { type: 'button', 'aria-label': 'Зарегистрироваться' }, 'Зарегистрироваться')
     ),
 }))
